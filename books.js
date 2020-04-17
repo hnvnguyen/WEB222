aa68ee4264ad2e24daf2896ed42612f4783b3506
Nguyen Huy Nhat Vy <35900593+hnvnguyen@users.noreply.github.com>
2020-04-17 13:38:57 -0400

    Add files via upload

---
+++ books.js
@@ -0,0 +1,264 @@
+window.books = [
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    picture: '',
+    price: '$0.00',
+    size: '781KB',
+    language: 'English',
+    description:
+      'Pride and Prejudice is one of the earliest and most influential examples of a novel of manners. By the 1700s, social classes and hierarchies had started to change in England.. (SparkNotes)',
+    url: 'https://www.gutenberg.org/files/1342/1342-0.txt',
+    categories: ['Romance', 'Fiction']
+  },
+  {
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    picture: '',
+    price: '$0.00',
+    size: '1.4MB',
+    language: 'English',
+    description:
+      'The Da Vinci Code raises the question of whether history books necessarily tell the only truth. The novel is full of reinterpretations of commonly told stories, such as those of Jesus’ life, the pentacle, and the Da Vinci fresco The Last Supper... (SparkNotes)',
+    url:
+      'https://ia800404.us.archive.org/9/items/TheDaVinciCode_201308/The%20Da%20Vinci%20Code.pdf',
+    categories: ['Mystery', 'Detective', 'Fiction']
+  },
+  {
+    title: '1984',
+    author: 'George Orwell',
+    picture: '',
+    price: '$0.00',
+    size: '1.3MB',
+    language: 'English',
+    description:
+      'In 1984, Orwell portrays the perfect totalitarian society, the most extreme realization imaginable of a modern-day government with absolute power. The title of the novel was meant to indicate to its readers in 1949 that the story represented a real possibility for the near future... (SparkNotes)',
+    url:
+      'https://ia800201.us.archive.org/8/items/NINETEENEIGHTY-FOUR1984ByGeorgeOrwellPDFAudioBook/1984.pdf',
+    categories: ['Dystopia', 'Fiction', 'Politics']
+  },
+  {
+    title: 'Brave New World',
+    author: 'Aldous Huxley',
+    picture: '',
+    price: '$0.00',
+    size: '579.9kB',
+    language: 'English',
+    description:
+      'In presenting a world where society has been perfected and people live happily and peacefully, Brave New World is an example of a utopian novel. Taking its name from Sir Thomas More’s 1516 novel Utopia, the genre of utopian novels posit that suffering can be eradicated through the perfection of society... (SparkNotes)',
+    url:
+      'https://ia600306.us.archive.org/24/items/ost-english-brave_new_world_aldous_huxley/Brave_New_World_Aldous_Huxley.pdf',
+    categories: ['Dystopia', 'Fiction', 'Politics']
+  },
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    picture: '',
+    price: '$0.00',
+    size: '746KB',
+    language: 'English',
+    description:
+      'On the surface, The Great Gatsby is a story of the thwarted love between a man and a woman. The main theme of the novel, however, encompasses a much larger, less romantic scope... (SparkNotes)',
+    url: 'https://www.planetebook.com/free-ebooks/the-great-gatsby.pdf',
+    categories: ['Tragedy', 'Fiction']
+  },
+  {
+    title: 'The Prince',
+    author: 'Niccolo',
+    picture: '',
+    price: '$0.00',
+    size: '835KB',
+    language: 'English',
+    description:
+      'The most revolutionary aspect of The Prince is its separation of politics and ethics. Classical political theory traditionally linked political law with a higher, moral law. In contrast, Machiavelli argues that political action must always be considered in light of its practical consequences rather than some lofty ideal... (SparkNotes)',
+    url: 'https://www.planetebook.com/free-ebooks/the-prince.pdf',
+    categories: ['Philosophy', 'Non-fiction']
+  },
+  {
+    title: 'Murder on the Orient Express',
+    author: 'Agatha Christie',
+    picture: '',
+    price: '$0.00',
+    size: '609KB',
+    language: 'English',
+    description:
+      'Hercule Poirot, a recurring Christie character, has become one of the most famous fictional detectives. Poirot is a retired Belgian police officer turned private detective. As a private detective he tours Europe and the Mid-East solving murder mysteries... (SparkNotes)',
+    url:
+      'https://www.pdf-archive.com/2017/06/15/murder-on-the-orient-express/murder-on-the-orient-express.pdf',
+    categories: ['Detective', 'Crime', 'Mystery', 'Fiction']
+  },
+  {
+    title: 'The Interpretation of Dreams',
+    author: 'Sigmund Freud',
+    picture: '',
+    price: '$0.00',
+    size: '1.2MB',
+    language: 'English',
+    description:
+      'This book introduces Freud\'s theory of the unconscious with respect to dream interpretation. Dreams, in Freud\'s view, were all forms of "wish-fulfillment" — attempts by the unconscious to resolve a conflict of some sort, whether something recent or something from the recesses of the past... (The Greatest Books)',
+    url: 'https://archive.org/stream/interpretationof1913freu/interpretationof1913freu_djvu.txt',
+    categories: ['Psychology', 'Non-fiction']
+  },
+  {
+    title: 'Confessions',
+    author: 'Saint Augustine',
+    picture: '',
+    price: '$0.00',
+    size: '959KB',
+    language: 'Latin',
+    description:
+      'Confessions is the name of an autobiographical work, consisting of 13 books, by St. Augustine of Hippo, written between AD 397 and AD 398... (The Greatest Books)',
+    url: 'https://www.gutenberg.org/files/33849/33849-8.txt',
+    categories: ['Autobiography', 'Non-fiction']
+  },
+  {
+    title: 'The Autobiography of Malcolm X',
+    author: 'Alex Haley and Malcolm X',
+    picture: '',
+    price: '$0.00',
+    size: '1.6MB',
+    language: 'English',
+    description:
+      "This book describes Malcolm X's upbringing in Michigan, his maturation to adulthood in Boston and New York, his time in prison, his conversion to Islam, his ministry, his travels to Africa and to Mecca,... (The Greatest Books)",
+    url:
+      'https://ia801609.us.archive.org/25/items/TheAutobiographyOfMalcomX/The_Autobiography_Of_MalcomX.pdf',
+    categories: ['Autobiography', 'Non-fiction']
+  },
+  {
+    title: "Cicero's Orations",
+    author: 'Marcus Tullius Cicero',
+    picture: '',
+    price: '$0.00',
+    size: '110KB',
+    language: 'Latin',
+    description:
+      'This is one of the best-documented events surviving from the ancient world, and has set the stage for classic political struggles pitting state security against civil liberties... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/226/226.txt',
+    categories: ['Politics', 'Non-fiction']
+  },
+  {
+    title: 'Odes and Epodes',
+    author: 'Horace',
+    picture: '',
+    price: '$0.00',
+    size: '165KB',
+    language: 'Latin',
+    description:
+      'Horace was another leading poet of the Roman world, known for his lyrical poetry, often amusing tone, and graceful composition. Writing between 65 BC and 8 BC, Horace penned 9 major works that survive to this day... (Brainscape Blog)',
+    url: 'https://www.gutenberg.org/files/9646/9646.txt',
+    categories: ['Poem', 'Fiction']
+  },
+  {
+    title: 'Silent Spring',
+    author: 'Rachel Carson',
+    picture: '',
+    price: '$0.00',
+    size: '967.9KB',
+    language: 'English',
+    description:
+      'Silent Spring is a book written by Rachel Carson and published by Houghton Mifflin in September 1962. The book is widely credited with helping launch the environmental movement... (The Greatest Books)',
+    url:
+      'https://ia902801.us.archive.org/13/items/fp_Silent_Spring-Rachel_Carson-1962/Silent_Spring-Rachel_Carson-1962.pdf',
+    categories: ['Environment', 'Non-fiction']
+  },
+  {
+    title: 'On the Origin of Species',
+    author: 'Charles Darwin',
+    picture: '',
+    price: '$0.00',
+    size: '948KB',
+    language: 'English',
+    description:
+      "Charles Darwin's On the Origin of Species, published on Thursday 24 November 1859, is a seminal work of scientific literature considered to be the foundation of evolutionary biology... (The Greatest Books)",
+    url: 'https://www.gutenberg.org/files/1228/1228.txt',
+    categories: ['Environment', 'Non-fiction']
+  },
+  {
+    title: 'Autobiography of Benjamin Franklin',
+    author: 'Benjamin Franklin',
+    picture: '',
+    price: '$0.00',
+    size: '456KB',
+    language: 'English',
+    description:
+      "Born 1706 in Boston, Benjamin Franklin was the 15th of his father's 17 children. He went to school as a child with the intent of becoming a minister, as his father, Josiah, intended. However, that idea was dropped after Franklin showed a keen interest in reading and writing... (SparkNotes)",
+    url: 'https://www.gutenberg.org/files/20203/20203.txt',
+    categories: ['Autobiography', 'Non-fiction']
+  },
+  {
+    title: 'Romeo and Juliet',
+    author: 'William Shakespeare',
+    picture: '',
+    price: '$0.00',
+    size: '175KB',
+    language: 'English',
+    description:
+      'Much of Romeo and Juliet involves the lovers’ struggles against public and social institutions that either explicitly or implicitly oppose the existence of their love... (SparkNotes)',
+    url: 'https://www.gutenberg.org/files/1112/1112.txt',
+    categories: ['Tragedy', 'Romance', 'Fiction']
+  },
+  {
+    title: 'The Mysterious Affair at Styles',
+    author: 'Agatha Christie',
+    picture: '',
+    price: '$0.00',
+    size: '353KB',
+    language: 'English',
+    description:
+      'Agatha Christie began working on The Mysterious Affair at Styles in 1916, writing most of it on Dartmoor. The character of Hercule Poirot was inspired by her experience working as a nurse... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/863/863-0.txt',
+    categories: ['Crime', 'Mystery', 'Detective', 'Fiction']
+  },
+  {
+    title: 'A Study in Scarlet',
+    author: 'Sir Arthur Conan Doyle',
+    picture: '',
+    price: '$0.00',
+    size: '261KB',
+    language: 'English',
+    description:
+      'A Study in Scarlet is an 1887 detective novel by Scottish author Arthur Conan Doyle. The story marks the first appearance of Sherlock Holmes and Dr. Watson, who would become the most famous detective duo in popular fiction... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/244/244.txt',
+    categories: ['Detective', 'Crime', 'Mystery', 'Fiction']
+  },
+  {
+    title: 'The Sign of the Four',
+    author: 'Sir Arthur Conan Doyle',
+    picture: '',
+    price: '$0.00',
+    size: '255KB',
+    language: 'English',
+    description:
+      'The Sign of the Four (1890), also called The Sign of Four, is the second novel featuring Sherlock Holmes written by Sir Arthur Conan Doyle... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/2097/2097-0.txt',
+    categories: ['Detective', 'Crime', 'Mystery', 'Fiction']
+  },
+  {
+    title: 'The Hound of the Baskervilles',
+    author: 'Sir Arthur Conan Doyle',
+    picture: '',
+    price: '$0.00',
+    size: '338KB',
+    language: 'English',
+    description:
+      'The Hound of the Baskervilles is the third of the four crime novels written by Sir Arthur Conan Doyle featuring the detective Sherlock Holmes... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/2852/2852.txt',
+    categories: ['Detective', 'Crime', 'Mystery', 'Fiction']
+  },
+  {
+    title: 'The Valley of Fear',
+    author: 'Sir Arthur Conan Doyle',
+    picture: '',
+    price: '$0.00',
+    size: '331KB',
+    language: 'English',
+    description:
+      'The Valley of Fear is the fourth and final Sherlock Holmes novel by Sir Arthur Conan Doyle... (Wikipedia)',
+    url: 'https://www.gutenberg.org/files/3289/3289.txt',
+    categories: ['Detective', 'Crime', 'Mystery', 'Fiction']
+  }
+];
+
+window.books.forEach(function(book) {
+  book.picture = book.title.replace(/[^a-z0-9]/gi, '') + '.jpg';
+});
---
+++ script.js
@@ -0,0 +1,133 @@
+/** Add any JavaScript you need to this file. */
+var books = window.books;
+
+/* An object containing every categories available and the amount of books belongs to each */
+let genreList = (function() {
+  let genres = {};
+  genres['All'] = books.length; // Add 'All' to the category
+  // Find the number of books a genre/category has
+  books.forEach(function(book) {
+    book.categories.forEach(function(genre) {
+      genres[genre] = genres[genre] ? genres[genre] + 1 : 1;
+    });
+  });
+  // Sort the genre list bases on the amount of books each genre has
+  let tempArr = []; // Turn the object into array for sorting ;))
+  for (const genre in genres) {
+    tempArr.push([genre, genres[genre]]);
+  }
+  tempArr.sort(function(a, b) {
+    return b[1] - a[1]; // Compare each genre's number of books
+  });
+  genres = {}; // Turn the array into object
+  tempArr.forEach(function(genre) {
+    genres[genre[0]] = genre[1];
+  });
+  return genres;
+})();
+
+/* Add the categories to the sidebar */
+function genresToSidebar() {
+  let nav = document.getElementById('nav-categories');
+  for (const genre in genreList) {
+    // Each is a list item <li>
+    let li = document.createElement('li');
+    li.id = `nav-category-${genre}`;
+    li.innerHTML = `<a href="#products">${genre} <span>${genreList[genre]}</span></a>`;
+    nav.appendChild(li);
+  }
+}
+
+// An array containing all the products that are currently displayed
+let products = (function() {
+  let arr = [];
+  for (let index = 0; index < books.length; index++) arr[index] = index;
+  return arr;
+})();
+
+/* Load and display all books on the window */
+function productsToMain() {
+  books.forEach(function(book, index) {
+    // Each book is contained inside a <div>
+    let wrapDiv = document.createElement('div');
+    wrapDiv.classList.add('div-wrapping');
+    wrapDiv.id = 'book-' + index;
+    // This is everything inside the book <div> ;))
+    wrapDiv.innerHTML = `
+        <figure>
+          <img src="images/${book.picture}" alt="Picture of ${book.title}" />
+          <figcaption>In ${book.language}</figcaption>
+        </figure>
+        <div class="book-info">
+          <h2><span class="title">${book.title}</span>&ensp;by ${book.author}</h2>
+          <p><b>Price: ${book.price}</b></p>
+          <p><b>Size:</b> ${book.size}</p><p>${book.description}</p>
+          <p><b>Categories:</b> ${book.categories.join(', ')}</p>
+          <div class="book-buttons">
+            <button type="button"><a target="_blank" href="${book.url}" title="${book.url}">
+              <b>Visit URL</b></a></button>
+          </div>
+        </div>`;
+    let main = document.querySelector('main');
+    main.appendChild(wrapDiv); // Add each <div> to the main page
+  });
+}
+
+/* Add or remove 'hidden' attribute of products based on the chosen category */
+function productsAddHide() {
+  for (let index = 0; index < books.length; index++) {
+    let element = document.querySelector(`#book-${index}`);
+    if (!products.includes(index)) {
+      element.classList.add('hidden');
+    } else {
+      element.classList.remove('hidden');
+    }
+  }
+}
+
+/** Change the books displayed based on which category the user chooses */
+function categoriesHandler() {
+  for (const genre in genreList) {
+    let cat = document.querySelector(`#nav-category-${genre}`);
+    // If the user clicks on a category
+    cat.onclick = function() {
+      let h1 = document.querySelector('main h1');
+      h1.innerHTML = genre; // Display the category name
+      books.forEach(function(book, index) {
+        // Store all books that belong to this genre into 'products' array
+        if (book.categories.includes(genre) || genre === 'All') {
+          if (!products.includes(index)) products.push(index);
+        } else if (products.indexOf(index) > -1) {
+          products.splice(products.indexOf(index), 1);
+        }
+      });
+      productsAddHide(); // Add or hide books from the main page
+      if (document.querySelector('.nav-button').offsetHeight !== 0) {
+        document.querySelector('#nav-categories').style.display = 'none';
+      }
+    };
+  }
+}
+
+/** Navigation drop-down */
+function navHandler() {
+  let button = document.querySelector('.nav-button');
+  let navP = document.querySelector('nav p');
+  if (button.style.display !== 'none') {
+    navP.style.cursor = 'pointer'; // Make it seems clickable :))
+  }
+  navP.addEventListener('click', function() {
+    if (button.style.display !== 'none') {
+      // if the category is chosen, hide the category list
+      let dropDown = document.querySelector('#nav-categories');
+      dropDown.style.display = dropDown.style.display === 'block' ? 'none' : 'block';
+    }
+  });
+}
+
+window.onload = function() {
+  genresToSidebar();
+  productsToMain();
+  categoriesHandler();
+  navHandler();
+};
